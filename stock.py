032af8cdfa39fbb8cba0348e6dd13837af30a502
Michael Nguyen <michaelalexnguyen@gmail.com>
2019-08-19 21:05:33 -0400

    added k in sysargs and some refactoring

--- stock.py
+++ stock.py
@@ -2,7 +2,7 @@ import csv
 import operator
 import math
 import random
-# array tuple sum 5 day price change, date
+import sys
 
 
 def get_change(current, previous):
@@ -17,9 +17,6 @@ def get_change(current, previous):
 def euclideanDistance(instance1, instance2, length):
     distance = 0
     for x in range(length):
-        # print(length)
-        # print(instance1)
-        # print(instance2)
         distance += pow((instance1[x] - instance2[x]), 2)
     return math.sqrt(distance)
 
@@ -47,7 +44,6 @@ def getResponse(neighbors):
             classVotes[response] = 1
     sortedVotes = sorted(classVotes.items(),
                          key=operator.itemgetter(1), reverse=True)
-    # print(sortedVotes)
     return sortedVotes
 
 
@@ -58,12 +54,13 @@ def getAccuracy(testSet, predictions):
             correct += 1
     return (correct/float(len(testSet))) * 100.0
 
+
 def getSentiment(result, testSet):
     predictedSentiment = ""
     trueSentiment = ""
     if result > 0:
         if result < .5:
-            predictedSentiment = "Barely bullish" 
+            predictedSentiment = "Barely bullish"
         else:
             predictedSentiment = "Bullish"
     else:
@@ -72,8 +69,8 @@ def getSentiment(result, testSet):
         else:
             predictedSentiment = "Bearish"
     if testSet > 0:
-        if testSet  < .5:
-            trueSentiment = "Barely bullish" 
+        if testSet < .5:
+            trueSentiment = "Barely bullish"
         else:
             trueSentiment = "Bullish"
     else:
@@ -82,7 +79,11 @@ def getSentiment(result, testSet):
         else:
             trueSentiment = "Bearish"
     return predictedSentiment, trueSentiment
+
+
 data = []
+tradingDays = int(sys.argv[1])
+
 with open('QQQ.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -91,50 +92,61 @@ with open('QQQ.csv') as csv_file:
     put = [None]*3
     reader_list = list(csv.reader(csv_file, delimiter=','))
     reader_list = reader_list[1:]
-    for idx,row in enumerate(reader_list):
+
+    # create formatted data-set
+    for idx, row in enumerate(reader_list):
         if (idx < len(reader_list)-4):
-            put[0] = round(get_change(float(reader_list[idx+4][4]),float(row[4])), 2)
+            put[0] = round(get_change(
+                float(reader_list[idx+4][4]), float(row[4])), 2)
             put[1] = row[0]
             put[2] = reader_list[idx+4][0]
             data.append(put)
             put = [None]*3
-    TEST = [x[0] for x in data[len(data)-10:]]
+    # create test data
+    TEST = [x[0] for x in data[len(data)-tradingDays:]]
     trainingSet = []
     testSet = []
     # turn into arrays of 5 (percent change over 5 days)
+    # randomly select test data and training data
     for i in range(len(data)):
-        if (len(data[i:i+10]) < 10):
+        if (len(data[i:i+tradingDays]) < tradingDays):
             break
         if random.random() < .67:
-            trainingSet.append([x[0] for x in data[i:i+10]])
+            trainingSet.append([x[0] for x in data[i:i+tradingDays]])
         else:
-            testSet.append([x[0] for x in data[i:i+10]])
+            testSet.append([x[0] for x in data[i:i+tradingDays]])
 
     predictions = []
-    k = 8
+    k = 10
     for x in range(len(testSet)):
         neighbors = getNeighbors(trainingSet, testSet[x], k)
         result = getResponse(neighbors)[0][0]
         predictions.append(result)
 
-        predictedSentiment, trueSentiment = getSentiment(result, testSet[x][-1])
+        predictedSentiment, trueSentiment = getSentiment(
+            result, testSet[x][-1])
     print(TEST)
     dd = getNeighbors(trainingSet, TEST, k)
     test_prediction = getResponse(dd)
     average = sum([x[0] for x in test_prediction])/len(test_prediction)
     sentiment = getSentiment(test_prediction[0][0], 0)[0]
     onlyValues = [x[0] for x in test_prediction]
-    print(sorted(test_prediction))
+    print(test_prediction)
+
+    prediction = 0
+    # if tie, give average
+    if (test_prediction[0][1] > test_prediction[1][1]):
+        prediction = test_prediction[0][0]
+    else:
+        predicion = (test_prediction[0][0] + test_prediction[1][0]) / 2
     generatedMovement = round((test_prediction[0][0] + average)/2)/2
-    # print(generatedMovement)
-    print('Prediction for next 5 trading days: ' + '{0:+}% / '.format(test_prediction[0][0]) + sentiment +', Historic average and range: ' + repr(average)+ ', ' + repr(max(onlyValues)-min(onlyValues)))
+    print('Prediction for next ' + str(tradingDays) + ' trading days: ' + '{0:+}% / '.format(
+        prediction) + sentiment + ', Historic average and range: ' + repr(average) + ', ' + repr(max(onlyValues)-min(onlyValues)))
 
     accuracy = getAccuracy(testSet, predictions)
     print('Accuracy: ' + repr(round(accuracy, 3)) + '%')
     cd = TEST[1:]
     cd.append(generatedMovement)
-    # print(cd)
-    getnew = getNeighbors(trainingSet, cd,k)
+    getnew = getNeighbors(trainingSet, cd, k)
     tt = getResponse(getnew)
-    # print(tt)
     print(f'Processed {len(data)} lines.')
